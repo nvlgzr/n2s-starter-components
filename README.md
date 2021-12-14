504ae610e71e928dbed12cb4ddb07f8e22fa31f7
Elbong Gearny <elbong@navelgazer.club>
2021-12-13 23:38:43 -0800

    Initial component set

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # n2s-starter-components
+
+A demo set of components for [_notion2svelte_](https://github.com/nvlgzr/notion2svelte).
+
+## Usage
+
+```
+cd <your-svelte-project>
+degit nvlgzr/n2s-starter-components/notion2svelte src/lib/
+```
---
+++ notion2svelte/Image.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let url = "Missing URL for image";
+</script>
+
+<figure>
+  <img width="100%" height="100%" src={url} />
+  <figcaption><slot style="margin-top: 0.75rem;" /></figcaption>
+</figure>
+
+<style>
+  figure {
+    color: gray;
+    background-color: whitesmoke;
+    padding: 1rem 1rem 3.5rem 1rem;
+    border: thin silver solid;
+    border-radius: 0.5rem;
+    margin: 0 auto 1rem auto;
+  }
+
+  figcaption {
+    margin-top: 0.5rem;
+  }
+</style>
