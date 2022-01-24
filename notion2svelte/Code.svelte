e2745a30621234e2f04b390ba9b9e6ca7a2f897d
Elbong Gearny <elbong@navelgazer.club>
2022-01-23 17:38:18 -0800

    pure refactor - save with auto-formatting

--- notion2svelte/Code.svelte
+++ notion2svelte/Code.svelte
@@ -1,6 +1,6 @@
 <script>
-	export let code = '';
-	export let language = '';
+  export let code = "";
+  export let language = "";
 </script>
 
 <pre>
@@ -9,29 +9,31 @@
 </pre>
 
 <style>
-  code, span {
-		font-family: 'SFMono-Regular', Menlo, Consolas, 'PT Mono', 'Liberation Mono', Courier, monospace;
+  code,
+  span {
+    font-family: "SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono",
+      Courier, monospace;
   }
 
-	pre {
-		position: relative;
-		line-height: normal;
-		display: block;
-		border-radius: 0.5rem;
-		margin: 0 2.5rem;
-		padding: 2.5rem;
-		overflow-x: auto;
-		white-space: pre-wrap;
-		word-wrap: break-word;
-		background: hsl(135, 88%, 16%);
-		color: hsl(120, 100%, 50%);
-	}
+  pre {
+    position: relative;
+    line-height: normal;
+    display: block;
+    border-radius: 0.5rem;
+    margin: 0 2.5rem;
+    padding: 2.5rem;
+    overflow-x: auto;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    background: hsl(135, 88%, 16%);
+    color: hsl(120, 100%, 50%);
+  }
 
-	span {
-		position: absolute;
-		right: 1rem;
-		bottom: 1rem;
-		font-size: 0.8em;
-		color: hsl(120, 50%, 50%);
-	}
+  span {
+    position: absolute;
+    right: 1rem;
+    bottom: 1rem;
+    font-size: 0.8em;
+    color: hsl(120, 50%, 50%);
+  }
 </style>
